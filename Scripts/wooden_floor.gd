716cc92e5420793b9c6f6db919bf6e3ae57f53a0
Patryk Czekaj <patryk13290@gmail.com>
2024-08-19 00:51:07 +0200

    wooden floor logic

--- Scripts/wooden_floor.gd
+++ Scripts/wooden_floor.gd
@@ -1,7 +1,13 @@
 extends StaticBody2D
 
+var broken: bool = false
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	if broken:
+		return
 	if body is Player:
-		if body.current_scale == 3:
+		print(body.current_just_landed)
+		if body.current_scale == 3 and body.current_just_landed > 0:
 			$AnimatedSprite2D.play("break")
 			$CollisionShape2D.set_deferred("disabled", true)
+			broken = true
