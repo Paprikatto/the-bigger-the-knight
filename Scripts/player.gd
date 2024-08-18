2aaf5fd22f0d5cceca43f406a1cdfb21fb0d6a74
Patryk Czekaj <patryk13290@gmail.com>
2024-08-18 02:02:58 +0200

    a lot
    size checks, working doors, sky background, jump buffer

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
+class_name Player
 
 
+@export var move_speed: float = 80
 
-@export var move_speed: float = 60
-
+#components
 @onready var animations: AnimationPlayer = $AnimationPlayer
 @onready var scale_animations: AnimationPlayer = $ScaleAnimations
 @onready var sprite: Sprite2D = $Sprite2D
@@ -11,13 +12,29 @@ extends CharacterBody2D
 #1 is small, 2 is normal, 3 is big
 var current_scale: int = 2
 
-@export var jump_force:float = 200
-var fall_speed:float = 5
-var max_fall_speed:float = 200
+#scale checks
+@export var big_check: Area2D
+@export var normal_check: Area2D
+
+#used in scaling
+@export var jump_factor: float = 1
+@export var move_factor: float = 1
+
+#jumping
+var jump_force:float = 200
+var gravity:float = 7
+var max_fall_speed:float = 300
+var fall_factor: float = 1.5
+
+##how early before landing on the ground I can hit space so as soon as i land, player immediately jumps
+@export var jump_buffer: float = 0.1
+var current_jump_buffer: float = 0
+
 
 var last_dist: float
 
 func _ready() -> void:
+	$ScaleAnimations.play("RESET")
 	GameManager.game_over.connect(on_game_over)
 	
 	velocity.x = move_speed
@@ -25,8 +42,10 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump"):
-		print("jump")
-		velocity.y = -jump_force
+		if is_on_floor():
+			jump()
+		else:
+			current_jump_buffer = jump_buffer
 	elif event.is_action_pressed("scale_up"):
 		scale_up()
 	elif event.is_action_pressed("scale_down"):
@@ -36,11 +55,14 @@ func _input(event: InputEvent) -> void:
 func _physics_process(_delta: float) -> void:
 	if not is_on_floor():
 		if velocity.y != max_fall_speed:
-			velocity.y += fall_speed
+			if velocity.y < 0:
+				velocity.y += gravity
+			else:
+				velocity.y += gravity * fall_factor
 	elif velocity.y > 5:
 		velocity.y = 0
 	
-	velocity.x = Input.get_axis("move_left","move_right") * move_speed
+	velocity.x = Input.get_axis("move_left","move_right") * move_speed * move_factor
 	
 	if velocity.x:
 		animations.play("walk")
@@ -50,17 +72,29 @@ func _physics_process(_delta: float) -> void:
 			sprite.flip_h = false
 	else:
 		animations.play("idle")
-		
-	
-	
 	
 	move_and_slide()
-	
+
+func _process(delta: float) -> void:
+	#timers
+	if(current_jump_buffer > 0):
+		current_jump_buffer -= delta
+		
+	#jump buffer
+	if is_on_floor() and current_jump_buffer > 0:
+		jump()
+
+func jump():
+	velocity.y = -jump_force * jump_factor
+
 #region Scaling
 func scale_up():
-	#TODO: check for blocking roof
 	if current_scale == 1:
-		scale_animations.play_backwards("normal_to_small")
+		if not normal_check.has_overlapping_bodies():
+			scale_animations.play_backwards("normal_to_small")
+	elif current_scale == 2:
+		if not big_check.has_overlapping_bodies():
+			scale_animations.play("normal_to_big")
 	else:
 		return
 		
@@ -70,6 +104,8 @@ func scale_down():
 	#animations
 	if current_scale == 2:
 		scale_animations.play("normal_to_small")
+	elif current_scale == 3:
+		scale_animations.play_backwards("normal_to_big")
 	else: 
 		return
 	
