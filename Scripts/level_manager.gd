1945573d22edf6dbd04fdc9ff3f8dfc0db49827b
Patryk Czekaj <patryk13290@gmail.com>
2024-08-18 13:35:44 +0200

    going to the next level

--- Scripts/level_manager.gd
+++ Scripts/level_manager.gd
@@ -3,10 +3,13 @@ class_name LevelManager
 
 @export var current_level_index: int = 0
 @export var levels: Array[PackedScene]
+@export var end_game_screen: PackedScene
 
 var current_level: Node2D
 
 func _ready() -> void:
+	GameManager.next_level.connect(next_level)
+	
 	current_level = levels[current_level_index].instantiate()
 	self.add_child(current_level)
 
@@ -15,3 +18,15 @@ func restart_level():
 	current_level = levels[current_level_index].instantiate()
 	self.add_child(current_level)
 	
+func next_level():
+	if levels.size() > current_level_index+1:
+		$NextLevelAnimation.play_anim(false)
+		current_level_index += 1
+		current_level.queue_free()
+		current_level = levels[current_level_index].instantiate()
+		current_level.tree_entered.connect(level_loaded, CONNECT_ONE_SHOT)
+		self.call_deferred("add_child",current_level)
+
+func level_loaded():
+	$NextLevelAnimation.play_anim(true)
+		
