90720bb5e9e149dae915455cb974218e339dfce1
jajszu <bartekjaje@gmail.com>
2024-08-19 17:22:40 +0200

    animation in cutscene

--- Scripts/dialog.gd
+++ Scripts/dialog.gd
@@ -1,43 +1,59 @@
 extends dialog_line
 class_name dialog
 
-@export var first : Node
-@export var second : Node
+@export var animation : AnimationPlayer
+
+var timer : Timer
 
 @export var label : Label
 
 @export var dialogTable : Array[dialog_line]
 
+var lastWho : Who
+
 var i : int = 0
+var numLetter = 0
 var max : int
+var letterDelay = .05
 
 func _ready():
+	timer = $Timer
 	max = dialogTable.size()
+	timer.start(letterDelay)
 	
 	if max > 0:
 		label.text = dialogTable[i].text
+		lastWho = dialogTable[i].who
 		if dialogTable[i].who == Who.FIRST:
-			first.visible = true
-			second.visible = false
+			animation.play("move_rev")
 		else:
-			first.visible = false
-			second.visible = true
+			animation.play("move")
 
 func _unhandled_input(event):
 	if event is InputEventKey and event.is_pressed():
 		i+=1
-		
+		numLetter = 0
+		label.visible_characters = numLetter
+		var sameSide = false
 		if i >= max:
 			#koniec cutscenki tutaj przejscie dalej
 			
 			pass
 		else:
-			if dialogTable[i].who == Who.FIRST:
-				first.visible = true
-				second.visible = false
+			if lastWho == dialogTable[i].who:
+				sameSide = true
+			lastWho = dialogTable[i].who
+			if dialogTable[i].who == Who.FIRST and !sameSide:
+				animation.play("move_rev")
 			else:
-				first.visible = false
-				second.visible = true
-				
+				if !sameSide:
+					animation.play("move")
 			label.text = dialogTable[i].text
-		
+
+
+func _on_timer_timeout():
+	if numLetter <= label.text.length():
+		numLetter+=1
+		label.visible_characters = numLetter
+		timer.start(letterDelay)
+			
