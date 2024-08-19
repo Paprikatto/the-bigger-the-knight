df3aa73beca6816c8d2d544377296ec19edbc87c
Patryk Czekaj <patryk13290@gmail.com>
2024-08-19 18:01:16 +0200

    Revert "Merge pull request #2 from Paprikatto/jajsz"
    This reverts commit 113d075d5c2c62998d933cb4efdefe672f2d49a1, reversing
    changes made to 1469ba4a18fe19eca59d5116a63429e16d46ae54.

--- Scripts/dialog.gd
+++ Scripts/dialog.gd
@@ -1,64 +1,42 @@
 extends dialog_line
 class_name dialog
 
-@export var animation : AnimationPlayer
-
-var timer : Timer
+@export var first : Node
+@export var second : Node
 
 @export var label : Label
 
 @export var dialogTable : Array[dialog_line]
 
-var lastWho : Who
-
 var i : int = 0
-
-var numLetter = 0
-var max : int
-var letterDelay = .05
-
-func _ready():
-	timer = $Timer
-	max = dialogTable.size()
-	timer.start(letterDelay)
-
 var max_dialog : int
 
-
+func _ready():
+	max_dialog = dialogTable.size()
 	
 	if max_dialog > 0:
 		label.text = dialogTable[i].text
-		lastWho = dialogTable[i].who
 		if dialogTable[i].who == Who.FIRST:
-			animation.play("move_rev")
+			first.visible = true
+			second.visible = false
 		else:
-			animation.play("move")
+			first.visible = false
+			second.visible = true
 
 func _unhandled_input(event):
 	if event is InputEventKey and event.is_pressed():
 		i+=1
-		numLetter = 0
-		label.visible_characters = numLetter
-		var sameSide = false
+		
 		if i >= max_dialog:
-
 			#koniec cutscenki tutaj przejscie dalej
 			GameManager.next_level.emit()
 		else:
-			if lastWho == dialogTable[i].who:
-				sameSide = true
-			lastWho = dialogTable[i].who
-			if dialogTable[i].who == Who.FIRST and !sameSide:
-				animation.play("move_rev")
+			if dialogTable[i].who == Who.FIRST:
+				first.visible = true
+				second.visible = false
 			else:
-				if !sameSide:
-					animation.play("move")
+				first.visible = false
+				second.visible = true
+				
 			label.text = dialogTable[i].text
-
-
-func _on_timer_timeout():
-	if numLetter <= label.text.length():
-		numLetter+=1
-		label.visible_characters = numLetter
-		timer.start(letterDelay)
-			
+		
